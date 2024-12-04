5c75da813e686c1c30c616ded786ad2cc4f542a7
swaraj <swaraj@griffy.app>
2024-12-04 15:24:28 +0530

    Allow cors

--- packages/server/src/middleware/cors.ts
+++ packages/server/src/middleware/cors.ts
@@ -1,15 +1,5 @@
 import expressCors from 'cors';
 import config from '../../../../config.json';
 import { wrapExpressHandler } from './wrapExpressHandler';
-import { NextApiRequest, NextApiResponse } from 'next';
 
-// Define the cors function
-export const cors = async (req: NextApiRequest, res: NextApiResponse) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    if (req.method === 'OPTIONS') {
-        res.status(204).end(); // Respond to preflight request
-        return;
-    }
-};
+export const cors = wrapExpressHandler(expressCors({ origin: '*', methods: ['GET', 'POST', 'OPTIONS'] }));
