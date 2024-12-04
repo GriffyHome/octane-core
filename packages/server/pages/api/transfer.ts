1f143534cf38a9281f06a4a86bbd3c6e3c9459e1
swaraj <swaraj@griffy.app>
2024-12-04 16:20:20 +0530

    Warn fix

--- packages/server/pages/api/transfer.ts
+++ packages/server/pages/api/transfer.ts
@@ -90,7 +90,7 @@ export async function signWithTokenFee(
     console.log("Sign Txn");
 
     // Prevent simple duplicate transactions using a hash of the message
-    let key = `transaction/${base58.encode(core.sha256(transaction.serializeMessage()))}`;
+    const key = `transaction/${base58.encode(core.sha256(transaction.serializeMessage()))}`;
     if (await cache.get(key)) throw new Error('duplicate transaction');
     await cache.set(key, true);
 
